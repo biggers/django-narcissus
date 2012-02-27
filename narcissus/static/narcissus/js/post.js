cad43a557c948c5fe9cb0bb2050d1bb43eec43ad
bkonkle <brandon@konkle.us>
2012-02-26 21:10:15 -0700

    Preliminary submit function - time to work on the backend

--- narcissus/static/narcissus/js/post.js
+++ narcissus/static/narcissus/js/post.js
@@ -22,79 +22,44 @@ Narcissus.PostView = Backbone.View.extend({
     },
 
     submitPost: function(event) {
-        /*
-         * Not yet converted to the Backbone-based structure
-         */
-
         var self = this;
-
-        self.clear_errors();
-
-        self.$form.backdrop({}, function() {
-            self.$form.spin();
-
-            $.ajax({
-                type: 'POST',
-                url: self.$form.attr('action'),
-                data: self.$form.serialize(),
-                success: function(data) {
-                    self.$form.spin(false);
-                    self.$form.backdrop();
-
-                    if (data.success) {
-                        self.valid_form(data);
-                    } else {
-                        self.invalid_form(data);
-                    }
-                },
-                error: function(data) {
-                    self.$form.spin(false);
-                    self.$form.backdrop();
-                },
-                dataType: 'json'
-            });
-        });
-
-        return false;
-    },
-
-    valid_form: function(data) {
-        /*
-         * Not yet converted to the Backbone-based structure
-         */
-        var self = this;
-
-        $('<div class="alert-message success" data-alert="alert">' +
-          '<a class="close" href="#">×</a><p>Your post was successful!</p></div>')
-            .prependTo('#' + self.postType + '-submit-post')
-            .fadeOut(4000, function() {
-                $(this).remove();
-            });
-    },
-
-    invalid_form: function(data) {
-        /*
-         * Not yet converted to the Backbone-based structure
-         */
-        var self = this;
-
-        $.each(data.errors, function(field, errors) {
-            $input = $('#id_' + self.postType + '_' + field);
-            $input.addClass('error').parent().parent().addClass('error');
-            $input.after('<span class="help-inline">' + errors.join(' ') + '</span>');
-        });
-    },
-
-    clear_errors: function(data) {
-        /*
-         * Not yet converted to the Backbone-based structure
-         */
-        var self = this;
+        event.preventDefault();
 
         $('div.input, div.input-prepend').each(function() {
             $(self).parent().removeClass('error');
             $(self).children('input, textarea, select').removeClass('error')
                 .next('span.help-inline').remove();
+        });
+
+        this.$el.backdrop({}, function() {
+            self.$el.spin();
+
+            if (typeof self.model === 'undefined') {
+                self.model = new self.modelClass(self.$el.serialize());
+            }
+            self.model.save({}, {
+                success: function(model, response) {
+                    self.$el.spin(false);
+                    self.$el.backdrop();
+
+                    $('<div class="alert-message success" data-alert="alert">' +
+                      '<a class="close" href="#">×</a><p>Your post was successful!</p></div>')
+                        .prependTo('#' + self.postType + '-submit-post')
+                        .fadeOut(4000, function() {
+                            $(this).remove();
+                        });
+                },
+                error: function(model, response) {
+                    self.$el.spin(false);
+                    self.$el.backdrop();
+
+                    $.each(response.errors, function(field, errors) {
+                        var $input = $('#id_' + self.postType + '_' + field);
+                        $input.addClass('error').parent().parent().addClass('error');
+                        $input.after('<span class="help-inline">' + errors.join(' ') + '</span>');
+                    });
+                }
+            });
         });
     }
 
