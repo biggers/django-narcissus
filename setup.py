6f75cb0f567d3d46e67a86df32ae18a7159c0638
unknown <Steve@Steve-PC.(none)>
2012-04-07 10:10:26 +0100

    fixup to setup.py to code to distribute all packages along with templates and static files

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from distutils.core import setup
+from distutils.command.install_data import install_data
+from distutils.command.install import INSTALL_SCHEMES
+import os
+import sys
 
 VERSION = __import__('narcissus').__version__
 
@@ -11,6 +15,51 @@ description = "A blogging app that helps you easily share status updates, "
 description += "links, photos, videos, and long-form articles on your personal"
 description += " website."
 
+# django appropriate code to distribute all packages along with templates and static files taken
+# from here: http://code.djangoproject.com/svn/django/trunk/setup.py
+
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+# Tell distutils not to put the data_files in platform-specific installation
+# locations. See here for an explanation:
+# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
+
+# Compile the list of packages available, because distutils doesn't have
+# an easy way to do this.
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir != '':
+    os.chdir(root_dir)
+project_dir = 'narcissus'
+
+for dirpath, dirnames, filenames in os.walk(project_dir):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'): del dirnames[i]
+    if '__init__.py' in filenames:
+        packages.append('.'.join(fullsplit(dirpath)))
+    elif filenames:
+        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
+
+# Small hack for working with bdist_wininst.
+# See http://mail.python.org/pipermail/distutils-sig/2004-August/004134.html
+if len(sys.argv) > 1 and sys.argv[1] == 'bdist_wininst':
+    for file_info in data_files:
+        file_info[0] = '\\PURELIB\\%s' % file_info[0]
 
 setup(
     name='django-narcissus',
@@ -20,7 +69,8 @@ setup(
     author='Lincoln Loop',
     license='License :: OSI Approved :: BSD License',
     url='http://github.com/lincolnloop/django-narcissus',
-    packages=['narcissus'],
+	packages=packages,
+    data_files = data_files,
     classifiers=[
         'Framework :: Django',
         'Intended Audience :: Developers',
